0f72c0fbdfef597b2534ff96e78581bea3dd5529
Anas Halim <Anas Halim>
2015-11-26 12:33:24 +0200

    Initiate The program

---
+++ StructureAndPointers_V1.c
@@ -0,0 +1,24 @@
+/*Assignment for "C for Embedded Course":
+----Create a struct called student of char name[10], int seatNumber.
+----Create an array of pointers to that struct
+----Create a function to print the content of all the students passing to it the 
+......array of pointer to struct that u created holding all the student structs ......created which we assumed to be 5!*/
+
+# include <stdio.h>
+# include <stdlib.h>
+struct student
+ {
+	 char name[10];
+	 int seatno;
+	
+};
+
+int main()
+{
+	
+	
+	
+	
+	
+	return 0;
+}
